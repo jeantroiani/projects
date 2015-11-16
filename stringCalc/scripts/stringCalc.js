7794648ca3ef24f020cec23b81c1afa717462f22
Jean Troiani <troiani@Jeans-MacBook-Pro.local>
2015-11-16 22:37:47 +0000

    removes custom delimeter but returns NaN still

--- stringCalc/scripts/stringCalc.js
+++ stringCalc/scripts/stringCalc.js
@@ -2,16 +2,28 @@
 
 class StringCalc {
 
-    stringToArray (string) {
+    stringToArray (string, customSplitter) {
+        if (customSplitter) {
+            return string.split(customSplitter);
+        }
         return string.split(/[\n,]/g);
-        return string.split(",");
+    }
+
+    getCustomDelimeter (stringInput) {
+        const endOfDelimeter = stringInput.indexOf("\n");
+        return stringInput.slice(2, endOfDelimeter - 1);
     }
 
     add(stringInput) {
+        let delimeter = "";
+        // console.log(typeof delimeter);
         if (stringInput == "" || stringInput == null) {
             return 0;
         } else {
-            let arrayOfNumbers = this.stringToArray(stringInput);
+            if (stringInput.indexOf("//") === 0) {
+                delimeter = this.getCustomDelimeter(stringInput);
+            }
+            let arrayOfNumbers = this.stringToArray(stringInput, delimeter);
             if (arrayOfNumbers.length == 1) {
                 return Number(stringInput[0]);
             } else {
--- stringCalc/scripts/tests/stringCalc.test.js
+++ stringCalc/scripts/tests/stringCalc.test.js
@@ -6,32 +6,38 @@ describe("A string calculator", function () {
     it("can be instantiated", function () {
         expect(typeof stringCalc == "object").toEqual(true);
     });
+    describe("method Add", function () {
+        it("can be called with an empty string and return zero", function () {
+            expect(stringCalc.add("")).toEqual(0);
+        });
 
-    it("can be called with an empty string and return zero", function () {
-        expect(stringCalc.add("")).toEqual(0);
-    });
+        it("can be called with one number and return same number", function () {
+            expect(stringCalc.add("1")).toEqual(1);
+        });
 
-    it("can be called with one number and return same number", function () {
-        expect(stringCalc.add("1")).toEqual(1);
-    });
+        it("can be called with 3 number and return 3", function () {
+            expect(stringCalc.add("3")).toEqual(3);
+        });
 
-    it("can be called with 3 number and return 3", function () {
-        expect(stringCalc.add("3")).toEqual(3);
-    });
+        it("can be called with two numbers separated by comma and returns the sum of them", function () {
+            expect(stringCalc.add("1,2")).toEqual(3);
+        });
 
-    it("can be called with two numbers separated by comma and returns the sum of them", function () {
-        expect(stringCalc.add("1,2")).toEqual(3);
-    });
+        it("can be called with two numbers, 2 and 3 separated by comma and returns the sum of them", function () {
+            expect(stringCalc.add("2,3")).toEqual(5);
+        });
 
-    it("can be called with two numbers, 2 and 3 separated by comma and returns the sum of them", function () {
-        expect(stringCalc.add("2,3")).toEqual(5);
-    });
+        it("can be called with any amount of numbers 1, 2 and 3 separated by comma and it returns the sum of them", function () {
+            expect(stringCalc.add("1,2,3")).toEqual(6);
+        });
 
-    it("can be called with any amount of numbers 1, 2 and 3 separated by comma and it returns the sum of them", function () {
-        expect(stringCalc.add("1,2,3")).toEqual(6);
-    });
-
-    it("can be called with any numbers separated by new lines, and returns the sum of them", function () {
-        expect(stringCalc.add("1\n2,3")).toEqual(6);
+        it("can be called with any numbers separated by new lines, and returns the sum of them", function () {
+            expect(stringCalc.add("1\n2,3")).toEqual(6);
+        });
+        describe("suppports different delimeter", function () {
+            it("when called with //", function () {
+                expect(stringCalc.add("//;\n1;2")).toEqual(3);
+            });
+        })
     });
 });
